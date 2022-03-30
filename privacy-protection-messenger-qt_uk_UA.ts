fdd6efddc6358cb2fd6164b464000c33c2cd35ca
imperzer0 <unconfigured@null.spigotmc.org>
2022-03-31 02:37:40 +0300

    completed ukrainian translation

--- privacy-protection-messenger-qt_uk_UA.ts
+++ privacy-protection-messenger-qt_uk_UA.ts
@@ -1,11 +1,71 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="uk_UA">
-    <context>
-        <name>MainWindow</name>
-        <message>
-            <source>MainWindow</source>
-            <translation>ГоловнеВікно</translation>
-        </message>
-    </context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Welcome to privacy protection messenger</source>
+        <translation>Раді вітати вас у privacy protection messenger</translation>
+    </message>
+    <message>
+        <source>&lt;p style=&quot;text-align: center; color: #e9eeee;&quot;&gt; Login &lt;/p&gt;</source>
+        <translation>&lt;p style=&quot;text-align: center; color: #e9eeee;&quot;&gt; Логін &lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>type your login here</source>
+        <translation>введіть свій логін тут</translation>
+    </message>
+    <message>
+        <source>&lt;p style=&quot;text-align: center;&quot;&gt; Password &lt;/p&gt;</source>
+        <translation>&lt;p style=&quot;text-align: center;&quot;&gt; Пароль &lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>type your password here</source>
+        <translation>введіть свій пароль тут</translation>
+    </message>
+    <message>
+        <source>Sign in</source>
+        <translation>Зареєструватися</translation>
+    </message>
+    <message>
+        <source>I&apos;m already registered</source>
+        <translation>Я вже зареєстрований</translation>
+    </message>
+    <message>
+        <source>Log in</source>
+        <translation>Увійти</translation>
+    </message>
+    <message>
+        <source>I&apos;m not registered yet</source>
+        <translation>Я ще не зареєстрований</translation>
+    </message>
+    <message>
+        <source>find user</source>
+        <translation>знайти користувача</translation>
+    </message>
+    <message>
+        <source>message</source>
+        <translation>повідомлення</translation>
+    </message>
+    <message>
+        <source>send</source>
+        <translation>надіслати</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Мова</translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation>Англійська</translation>
+    </message>
+    <message>
+        <source>Ukrainian</source>
+        <translation>Українська</translation>
+    </message>
+    <message>
+        <source>MainWindow</source>
+        <translation>ГоловнеВікно</translation>
+    </message>
+</context>
 </TS>
